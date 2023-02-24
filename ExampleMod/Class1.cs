c445d4a8633a5a5dcf820aaeae470be62c5ec2e7
Rookie <test>
2023-02-25 00:20:16 +0200

    Example Mod

---
+++ ExampleMod/Class1.cs
@@ -0,0 +1,13 @@
+﻿using SpaceWarp.API;
+
+namespace ExampleMod
+{
+    [MainMod]
+    public class ExampleModRunner : Mod
+    {
+        public override void Initialize()
+        {
+            Logger.Info("Mod is initialized");
+        }
+    }
+}
