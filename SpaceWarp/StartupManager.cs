c9bc3fba4a57b1e0891ec16a4c8a411b675d1f56
Lexi <arallen3660@gmail.com>
2023-02-24 14:36:13 -0500

    Fixed incorrect position of "StartupManager.cs"

---
+++ SpaceWarp/StartupManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using HarmonyLib;
+using System.IO;
+
+namespace SpaceWarp
+{
+    public static class StartupManager
+    {
+        public const string MODS_FOLDER_NAME = "Mods";
+
+        public static void OnGameStarted()
+        {
+            Console.WriteLine("[Space Warp] Loaded");
+            string modsFolder = Application.dataPath + "/" + MODS_FOLDER_NAME;
+            // Create the mods directory if one does not yet exist
+            Console.WriteLine("[Space Warp] ");
+            Directory.CreateDirectory(modsFolder);
+        }
+    }
+}
