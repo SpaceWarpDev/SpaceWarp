117d7c98ce97461ef84ab06d1f7574592feeb896
Jan Bureš <jan.bures@live.com>
2023-02-26 21:35:12 +0100

    SpaceWarp BepInEx integration

--- SpaceWarp/Entrypoint.cs
+++ SpaceWarp/Entrypoint.cs
@@ -1,48 +1,27 @@
+using BepInEx;
 using HarmonyLib;
 using KSP.Logging;
 using SpaceWarp.UI;
 using System.Reflection;
-using UnityEngine.SceneManagement;
 
-namespace Doorstop
+namespace SpaceWarp
 {
-    public class Entrypoint
+    static class Package
     {
-        private static bool _patched;
-     
-        private const string HARMONY_PACKAGE_URL = "com.github.x606.spacewarp";
-        
-        /// <summary>
-        /// EntryPoint for Spacewarp, called from Doorstop
-        /// </summary>
-        public static void Start()
-        {
-            SceneManager.sceneLoaded += OnSceneLoaded;
+        public const string URL = "com.github.x606.spacewarp";
+        public const string NAME = "SpaceWarp";
+        public const string VERSION = "0.1.1";
+    }
 
+    [BepInPlugin(Package.URL, Package.NAME, Package.VERSION)]
+    public class SpaceWarpEntrypoint : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
             KspLogManager.AddLogCallback(SpaceWarpConsoleLogListener.LogCallback);
-        }
 
-        /// <summary>
-        /// Add OnGameStarted as postfix to StartGame
-        /// </summary>
-        static void OnSceneLoaded(Scene unused1, LoadSceneMode unused2)
-        {
-            if (!_patched)
-            {
-                InitializePatches();
-                _patched = true;
-            }
-        }
-
-        /// <summary>
-        /// Initializes Harmony
-        /// </summary>
-
-        static void InitializePatches()
-        {
-            Harmony harmony = new Harmony(HARMONY_PACKAGE_URL);
-
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Logger.LogInfo($"Plugin {Package.NAME} is loaded!");
         }
     }
 }
