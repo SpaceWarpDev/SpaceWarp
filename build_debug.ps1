f28a74a0a589d85f090d2eb5806aa80a765255a0
Lexi <arallen3660@gmail.com>
2023-02-26 10:55:30 -0500

    Added mod folder to build output

--- build_debug.ps1
+++ build_debug.ps1
@@ -5,5 +5,6 @@ dotnet build SpaceWarp/SpaceWarp.csproj -c Debug
 Copy-Item ./SpaceWarp/bin/Debug/SpaceWarp.dll ./build/SpaceWarp/core
 Copy-Item ./SpaceWarp/bin/Debug/SpaceWarp.pdb ./build/SpaceWarp/core
 Copy-Item ./SpaceWarp/bin/Debug/0Harmony.dll ./build/SpaceWarp/core
-mkdiR ./build/SpaceWarp/assets/bundles
+mkdir ./build/SpaceWarp/assets/bundles
+mkdir ./build/SpaceWarp/Mods
 Get-ChildItem ./Bundles -Filter *.bundle | Copy-Item -Destination ./build/SpaceWarp/assets/bundles/ -Force -PassThru
--- build_release.ps1
+++ build_release.ps1
@@ -4,5 +4,6 @@ Copy-Item ./Doorstop/* ./build
 dotnet build SpaceWarp/SpaceWarp.csproj -c Release
 Copy-Item ./SpaceWarp/bin/Release/SpaceWarp.dll ./build/SpaceWarp/core
 Copy-Item ./SpaceWarp/bin/Release/0Harmony.dll ./build/SpaceWarp/core
-mkdiR ./build/SpaceWarp/assets/bundles
+mkdir ./build/SpaceWarp/assets/bundles
+mkdir ./build/SpaceWarp/Mods
 Get-ChildItem ./Bundles -Filter *.bundle | Copy-Item -Destination ./build/SpaceWarp/assets/bundles/ -Force -PassThru
