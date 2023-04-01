b7b6f093ad61436b1827f883b389678cf4d5e122
Sen <sen@naraku>
2023-04-01 10:31:50 +0100

    Add an API for adding items to SequentialFlows
    Provides an API for adding new FlowItems to the game's
    SequentialFlows in StartBootstrap/StartGame, PrivateLoadCommon,
    and PrivateSaveCommon.
    
    Arguments passed to the creation of those flows are available when
    using the Generic methods, by taking the objects in the FlowAction's
    constructor.

---
+++ SpaceWarp/Patching/CodeGenerationUtilities.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceWarp.Patching;
+
+internal static class CodeGenerationUtilities
+{
+    /// <summary>
+    ///     Creates a CodeInstruction that pushed an integer to the stack.
+    /// </summary>
+    /// <param name="i">The integer to push</param>
+    /// <returns>An new CodeInstruction to push i</returns>
+    internal static CodeInstruction PushIntInstruction(int i)
+    {
+        switch (i) {
+            case 0: return new(OpCodes.Ldc_I4_0);
+            case 1: return new(OpCodes.Ldc_I4_1);
+            case 2: return new(OpCodes.Ldc_I4_2);
+            case 3: return new(OpCodes.Ldc_I4_3);
+            case 4: return new(OpCodes.Ldc_I4_4);
+            case 5: return new(OpCodes.Ldc_I4_5);
+            case 6: return new(OpCodes.Ldc_I4_6);
+            case 7: return new(OpCodes.Ldc_I4_7);
+            case 8: return new(OpCodes.Ldc_I4_8);
+            default: return new(OpCodes.Ldc_I4, i);
+        }
+    }
+}
