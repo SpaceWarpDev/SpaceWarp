c59229afeaa6b0272f5bd28ef7bd1cbda15f0cd4
Jan Bureš <jan.bures@live.com>
2023-05-09 00:15:29 +0200

    Localization of UITK mod list

---
+++ SpaceWarp/UI/Controls/IntlFoldout.cs
@@ -0,0 +1,32 @@
+﻿using I2.Loc;
+using UnityEngine.UIElements;
+
+namespace SpaceWarp.UI.Controls;
+
+public class IntlFoldout: Foldout
+{
+    public new class UxmlFactory : UxmlFactory<IntlFoldout, UxmlTraits>
+    {
+    }
+
+    public new class UxmlTraits : Foldout.UxmlTraits
+    {
+        private UxmlStringAttributeDescription m_Text = new UxmlStringAttributeDescription { name = "text" };
+
+        public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+        {
+            base.Init(ve, bag, cc);
+            var foldout = (IntlFoldout) ve;
+            foldout._localizedText = m_Text.GetValueFromBag(bag, cc);
+            foldout.text = foldout._localizedText;
+        }
+    }
+
+    private LocalizedString _localizedText;
+
+    public new string text
+    {
+        get => hierarchy.Children().OfType<Toggle>().First().text;
+        set => hierarchy.Children().OfType<Toggle>().First().text = value;
+    }
+}
