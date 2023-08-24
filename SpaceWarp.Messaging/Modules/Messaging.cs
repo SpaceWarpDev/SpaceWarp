c8e70fe80b7feab09c01f9a73d11331fe87a08e9
Lexi <arallen3660@gmail.com>
2023-08-24 16:48:09 -0400

    Start messaging project

---
+++ SpaceWarp.Messaging/Modules/Messaging.cs
@@ -0,0 +1,22 @@
+﻿namespace SpaceWarp.Modules;
+
+public class Messaging : SpaceWarpModule
+{
+    public override string Name => "SpaceWarp.Messaging";
+    public override void LoadModule()
+    {
+        
+    }
+
+    public override void PreInitializeModule()
+    {
+    }
+
+    public override void InitializeModule()
+    {
+    }
+
+    public override void PostInitializeModule()
+    {
+    }
+}
