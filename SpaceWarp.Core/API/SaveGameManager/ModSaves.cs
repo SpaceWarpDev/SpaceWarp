c8285183265a501865e55d63c302af842c055319
Lexi <arallen3660@gmail.com>
2023-11-07 08:48:16 -0500

    Make the onSave/onLoad parameters default to to null

--- SpaceWarp.Core/API/SaveGameManager/ModSaves.cs
+++ SpaceWarp.Core/API/SaveGameManager/ModSaves.cs
@@ -18,11 +18,11 @@ public static class ModSaves
     /// </summary>
     /// <typeparam name="T">Any object</typeparam>
     /// <param name="modGuid">Your mod GUID. Or, technically, any kind of string can be passed here, but what is mandatory is that it's unique compared to what other mods will use.</param>    
-    /// <param name="onSave">Function that will execute when a SAVE event is triggered. 'NULL' is also valid here if you don't need a callback.</param>
-    /// <param name="onLoad">Function that will execute when a LOAD event is triggered. 'NULL' is also valid here if you don't need a callback.</param>
+    /// <param name="onSave">Function that will execute when a SAVE event is triggered. Defaults to null or no callback.</param>
+    /// <param name="onLoad">Function that will execute when a LOAD event is triggered. Defaults to null or no callback.</param>
     /// <param name="saveData">Your object that will be saved to a save file during a save event and that will be updated when a load event pulls new data. Ensure that a new instance of this object is NOT created after registration.</param>
     /// <returns>T saveData object you passed as a parameter, or a default instance of object T if you didn't pass anything</returns>
-    public static T RegisterSaveLoadGameData<T>(string modGuid, Action<T> onSave, Action<T> onLoad, T saveData = default)
+    public static T RegisterSaveLoadGameData<T>(string modGuid, Action<T> onSave = null, Action<T> onLoad = null, T saveData = default)
     {
         // Check if this GUID is already registered
         if (InternalPluginSaveData.Find(p => p.ModGuid == modGuid) != null)
@@ -71,11 +71,11 @@ public static class ModSaves
     /// </summary>
     /// <typeparam name="T"> Any object</typeparam>
     /// <param name="modGuid">Your mod GUID. Or, technically, any kind of string can be passed here, but what is mandatory is that it's unique compared to what other mods will use.</param>    
-    /// <param name="onSave">Function that will execute when a SAVE event is triggered. 'NULL' is also valid here if you don't need a callback.</param>
-    /// <param name="onLoad">Function that will execute when a LOAD event is triggered. 'NULL' is also valid here if you don't need a callback.</param>
+    /// <param name="onSave">Function that will execute when a SAVE event is triggered. Defaults to null or no callback.</param>
+    /// <param name="onLoad">Function that will execute when a LOAD event is triggered. Defaults to null or no callback.</param>
     /// <param name="saveData">Your object that will be saved to a save file during a save event and that will be updated when a load event pulls new data. Ensure that a new instance of this object is NOT created after registration.</param>
     /// <returns>T saveData object you passed as a parameter, or a default instance of object T if you didn't pass anything</returns>
-    public static T ReregisterSaveLoadGameData<T>(string modGuid, Action<T> onSave, Action<T> onLoad, T saveData = default(T))
+    public static T ReregisterSaveLoadGameData<T>(string modGuid, Action<T> onSave = null, Action<T> onLoad = null, T saveData = default(T))
     {
         UnRegisterSaveLoadGameData(modGuid);
         return RegisterSaveLoadGameData(modGuid, onSave, onLoad, saveData);
