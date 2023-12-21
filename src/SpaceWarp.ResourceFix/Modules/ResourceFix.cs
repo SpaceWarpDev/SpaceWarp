57b103149422ac4fa8ec27e1744b9bee092a8e82
Lexi <arallen3660@gmail.com>
2023-12-21 17:02:45 -0500

    Fix some dumbassery

--- src/SpaceWarp.ResourceFix/Modules/ResourceFix.cs
+++ src/SpaceWarp.ResourceFix/Modules/ResourceFix.cs
@@ -4,24 +4,20 @@ namespace SpaceWarp.ResourceFix.Modules;
 
 public class ResourceFix : SpaceWarpModule
 {
-    public override string Name { get; }
+    public override string Name => "SpaceWarp.ResourceFix";
     public override void LoadModule()
     {
-        throw new NotImplementedException();
     }
 
     public override void PreInitializeModule()
     {
-        throw new NotImplementedException();
     }
 
     public override void InitializeModule()
     {
-        throw new NotImplementedException();
     }
 
     public override void PostInitializeModule()
     {
-        throw new NotImplementedException();
     }
 }
