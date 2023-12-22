f2871af4335cb2ff7c33913ca36800365bb9c8f3
Lexi <arallen3660@gmail.com>
2023-12-21 20:07:28 -0500

    Revert "Fix some dumbassery"
    This reverts commit 57b103149422ac4fa8ec27e1744b9bee092a8e82.

--- src/SpaceWarp.ResourceFix/Modules/ResourceFix.cs
+++ src/SpaceWarp.ResourceFix/Modules/ResourceFix.cs
@@ -4,20 +4,24 @@ namespace SpaceWarp.ResourceFix.Modules;
 
 public class ResourceFix : SpaceWarpModule
 {
-    public override string Name => "SpaceWarp.ResourceFix";
+    public override string Name { get; }
     public override void LoadModule()
     {
+        throw new NotImplementedException();
     }
 
     public override void PreInitializeModule()
     {
+        throw new NotImplementedException();
     }
 
     public override void InitializeModule()
     {
+        throw new NotImplementedException();
     }
 
     public override void PostInitializeModule()
     {
+        throw new NotImplementedException();
     }
 }
