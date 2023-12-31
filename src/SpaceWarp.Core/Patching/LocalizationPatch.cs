6ee244309076421408673a1cae1cc56ad1d7c24b
Jan Bureš <jan.bures@live.com>
2023-12-31 22:59:49 +0100

    https://youtu.be/0xyxtzD54rM

--- src/SpaceWarp.Core/Patching/LocalizationPatch.cs
+++ src/SpaceWarp.Core/Patching/LocalizationPatch.cs
@@ -8,7 +8,7 @@ namespace SpaceWarp.Patching;
 /// are not skipped when looking for a fallback translation.
 /// </summary>
 [HarmonyPatch]
-public class LocalizationPatch
+public static class LocalizationPatch
 {
     [HarmonyPatch(typeof(LanguageSourceData), nameof(LanguageSourceData.TryGetTranslation))]
     [HarmonyPostfix]
@@ -37,13 +37,86 @@ public class LocalizationPatch
             return;
         }
 
-        if (!__instance.TryGetFallbackTranslation(termData, out var translation, -1,
-                overrideSpecialization, skipDisabled))
+        if (!__instance.TryGetFallbackTranslation(
+                termData,
+                out var translation,
+                -1,
+                overrideSpecialization,
+                skipDisabled)
+            )
         {
             return;
         }
 
         Translation = translation;
         __result = true;
+    }
+
+    [HarmonyPatch(typeof(LanguageSourceData), nameof(LanguageSourceData.TryGetFallbackTranslation))]
+    [HarmonyPrefix]
+    private static bool TryGetFallbackTranslation(
+        // ReSharper disable once InconsistentNaming
+        LanguageSourceData __instance,
+        // ReSharper disable once InconsistentNaming
+        ref bool __result,
+        TermData termData,
+        // ReSharper disable once InconsistentNaming
+        out string Translation,
+        int langIndex,
+        string overrideSpecialization = null,
+        bool skipDisabled = false)
+    {
+        var str = langIndex != -1 && __instance.mLanguages.Count > langIndex
+            ? __instance.mLanguages[langIndex].Code
+            : null;
+        if (!string.IsNullOrEmpty(str))
+        {
+            if (str.Contains('-'))
+            {
+                str = str[..str.IndexOf('-')];
+            }
+
+            for (var index = 0; index < __instance.mLanguages.Count; ++index)
+            {
+                if (index == langIndex ||
+                    !__instance.mLanguages[index].Code.StartsWith(str, StringComparison.Ordinal) ||
+                    (skipDisabled && !__instance.mLanguages[index].IsEnabled()))
+                {
+                    continue;
+                }
+
+                Translation = termData.GetTranslation(index, overrideSpecialization, true);
+                if (string.IsNullOrEmpty(Translation))
+                {
+                    continue;
+                }
+
+                __result = true;
+                return false;
+            }
+        }
+
+        for (var index = 0; index < __instance.mLanguages.Count; ++index)
+        {
+            if (index == langIndex ||
+                (skipDisabled && !__instance.mLanguages[index].IsEnabled()) ||
+                (str != null && __instance.mLanguages[index].Code.StartsWith(str, StringComparison.Ordinal)))
+            {
+                continue;
+            }
+
+            Translation = termData.GetTranslation(index, overrideSpecialization, true);
+            if (string.IsNullOrEmpty(Translation))
+            {
+                continue;
+            }
+
+            __result = true;
+            return false;
+        }
+
+        Translation = null;
+        __result = false;
+        return false;
     }
 }
