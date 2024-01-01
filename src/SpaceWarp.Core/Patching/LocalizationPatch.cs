c08c5d53c104177a689ece0b5fb138f05304aaee
Jan Bureš <jan.bures@live.com>
2024-01-01 14:22:55 +0100

    Fixed localization patch

--- src/SpaceWarp.Core/Patching/LocalizationPatch.cs
+++ src/SpaceWarp.Core/Patching/LocalizationPatch.cs
@@ -1,122 +1,250 @@
 ﻿using HarmonyLib;
 using I2.Loc;
+using UnityEngine;
 
 namespace SpaceWarp.Patching;
 
 /// <summary>
-/// Patches the localization system so that localization sources that don't contain the current language
-/// are not skipped when looking for a fallback translation.
+/// Patches the localization system to properly find fallback translations.
 /// </summary>
 [HarmonyPatch]
-public static class LocalizationPatch
+internal static class LocalizationPatch
 {
-    [HarmonyPatch(typeof(LanguageSourceData), nameof(LanguageSourceData.TryGetTranslation))]
-    [HarmonyPostfix]
-    private static void TryGetTranslationPatch(
-        // ReSharper disable once InconsistentNaming
-        LanguageSourceData __instance,
-        // ReSharper disable once InconsistentNaming
+    [HarmonyPatch(
+        typeof(LocalizationManager),
+        nameof(LocalizationManager.TryGetTranslation),
+        [
+            typeof(string), typeof(string), typeof(bool), typeof(int), typeof(bool), typeof(bool),
+            typeof(GameObject), typeof(string), typeof(bool), typeof(string)
+        ],
+        [
+            ArgumentType.Normal, ArgumentType.Out, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal,
+            ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal
+        ]
+    )]
+    [HarmonyPrefix]
+    // ReSharper disable InconsistentNaming
+    private static bool LocalizationManagerTryGetTranslationPrefix(
         ref bool __result,
-        string term,
-        // ReSharper disable once InconsistentNaming
-        ref string Translation,
+        string Term,
+        out string Translation,
+        bool FixForRTL = true,
+        int maxLineLengthForRTL = 0,
+        bool ignoreRTLnumbers = true,
+        bool applyParameters = false,
+        GameObject localParametersRoot = null,
         string overrideLanguage = null,
-        string overrideSpecialization = null,
-        bool skipDisabled = false,
-        bool allowCategoryMistmatch = false)
+        bool allowLocalizedParameters = true,
+        string overrideSpecialization = null
+    )
     {
-        if (__result && !string.IsNullOrEmpty(Translation) && !Translation.Equals(term))
-        {
-            return;
-        }
+        // ReSharper restore InconsistentNaming
+        __result = TryGetTranslation(
+            Term,
+            out Translation,
+            FixForRTL,
+            maxLineLengthForRTL,
+            ignoreRTLnumbers,
+            applyParameters,
+            localParametersRoot,
+            overrideLanguage,
+            allowLocalizedParameters,
+            overrideSpecialization
+        );
 
-        var termData = __instance.GetTermData(term, allowCategoryMistmatch);
-        if (__instance.OnMissingTranslation != LanguageSourceData.MissingTranslationAction.Fallback ||
-            termData == null)
-        {
-            return;
-        }
-
-        if (!__instance.TryGetFallbackTranslation(
-                termData,
-                out var translation,
-                -1,
-                overrideSpecialization,
-                skipDisabled)
-            )
-        {
-            return;
-        }
-
-        Translation = translation;
-        __result = true;
+        return false;
     }
 
-    [HarmonyPatch(typeof(LanguageSourceData), nameof(LanguageSourceData.TryGetFallbackTranslation))]
-    [HarmonyPrefix]
-    private static bool TryGetFallbackTranslation(
-        // ReSharper disable once InconsistentNaming
-        LanguageSourceData __instance,
-        // ReSharper disable once InconsistentNaming
-        ref bool __result,
-        TermData termData,
-        // ReSharper disable once InconsistentNaming
-        out string Translation,
-        int langIndex,
-        string overrideSpecialization = null,
-        bool skipDisabled = false)
+    private static bool TryGetTranslation(
+        string term,
+        out string translation,
+        bool fixForRtl,
+        int maxLineLengthForRtl,
+        bool ignoreRtlNumbers,
+        bool applyParameters,
+        GameObject localParametersRoot,
+        string overrideLanguage,
+        bool allowLocalizedParameters,
+        string overrideSpecialization
+    )
     {
-        var str = langIndex != -1 && __instance.mLanguages.Count > langIndex
-            ? __instance.mLanguages[langIndex].Code
-            : null;
-        if (!string.IsNullOrEmpty(str))
+        translation = null;
+        if (string.IsNullOrEmpty(term))
         {
-            if (str.Contains('-'))
-            {
-                str = str[..str.IndexOf('-')];
-            }
-
-            for (var index = 0; index < __instance.mLanguages.Count; ++index)
-            {
-                if (index == langIndex ||
-                    !__instance.mLanguages[index].Code.StartsWith(str, StringComparison.Ordinal) ||
-                    (skipDisabled && !__instance.mLanguages[index].IsEnabled()))
-                {
-                    continue;
-                }
-
-                Translation = termData.GetTranslation(index, overrideSpecialization, true);
-                if (string.IsNullOrEmpty(Translation))
-                {
-                    continue;
-                }
-
-                __result = true;
-                return false;
-            }
-        }
-
-        for (var index = 0; index < __instance.mLanguages.Count; ++index)
-        {
-            if (index == langIndex ||
-                (skipDisabled && !__instance.mLanguages[index].IsEnabled()) ||
-                (str != null && __instance.mLanguages[index].Code.StartsWith(str, StringComparison.Ordinal)))
-            {
-                continue;
-            }
-
-            Translation = termData.GetTranslation(index, overrideSpecialization, true);
-            if (string.IsNullOrEmpty(Translation))
-            {
-                continue;
-            }
-
-            __result = true;
             return false;
         }
 
-        Translation = null;
-        __result = false;
+        if (LocalizationManager.DebugLocalizationIsOn)
+        {
+            translation = term;
+            return true;
+        }
+
+        LocalizationManager.InitializeIfNeeded();
+
+        foreach (var source in LocalizationManager.Sources)
+        {
+            if (!TryGetTranslationFromSource(
+                    source,
+                    term,
+                    out translation,
+                    overrideLanguage,
+                    overrideSpecialization
+                ))
+            {
+                continue;
+            }
+
+            if (applyParameters)
+            {
+                LocalizationManager.ApplyLocalizationParams(
+                    ref translation,
+                    localParametersRoot,
+                    allowLocalizedParameters
+                );
+            }
+
+            if (LocalizationManager.IsRight2Left & fixForRtl)
+            {
+                translation = LocalizationManager.ApplyRTLfix(translation, maxLineLengthForRtl, ignoreRtlNumbers);
+            }
+
+            return true;
+        }
+
+        return TryGetFallback(
+            term,
+            ref translation,
+            fixForRtl,
+            maxLineLengthForRtl,
+            ignoreRtlNumbers,
+            applyParameters,
+            localParametersRoot,
+            allowLocalizedParameters,
+            overrideSpecialization
+        );
+    }
+
+    private static bool TryGetTranslationFromSource(
+        LanguageSourceData source,
+        string term,
+        out string translation,
+        string overrideLanguage = null,
+        string overrideSpecialization = null,
+        bool skipDisabled = false,
+        bool allowCategoryMistmatch = false
+    )
+    {
+        var languageIndex = source.GetLanguageIndex(
+            overrideLanguage ?? LocalizationManager.CurrentLanguage,
+            SkipDisabled: false
+        );
+
+        if (languageIndex >= 0 && (!skipDisabled || source.mLanguages[languageIndex].IsEnabled()))
+        {
+            var termData = source.GetTermData(term, allowCategoryMistmatch);
+            if (termData != null)
+            {
+                translation = termData.GetTranslation(languageIndex, overrideSpecialization, true);
+                if (translation == "---")
+                {
+                    translation = string.Empty;
+                    return true;
+                }
+
+                if (!string.IsNullOrEmpty(translation))
+                {
+                    return true;
+                }
+            }
+        }
+
+        translation = null;
+        return false;
+    }
+
+    private static bool TryGetFallback(
+        string term,
+        ref string translation,
+        bool fixForRtl,
+        int maxLineLengthForRtl,
+        bool ignoreRtlNumbers,
+        bool applyParameters,
+        GameObject localParametersRoot,
+        bool allowLocalizedParameters,
+        string overrideSpecialization
+    )
+    {
+        if (!string.IsNullOrEmpty(translation) && !translation.Equals(term))
+        {
+            return true;
+        }
+
+        foreach (var source in LocalizationManager.Sources)
+        {
+            switch (source.OnMissingTranslation)
+            {
+                case LanguageSourceData.MissingTranslationAction.ShowWarning:
+                    translation = "<!-Missing Translation [" + term + "]-!>";
+                    return true;
+                case LanguageSourceData.MissingTranslationAction.Empty:
+                    translation = string.Empty;
+                    return true;
+                case LanguageSourceData.MissingTranslationAction.ShowTerm:
+                    translation = term;
+                    return true;
+            }
+
+            if (!TryGetFallbackFromSource(source, term, out var fallback, overrideSpecialization))
+            {
+                continue;
+            }
+
+            if (applyParameters)
+            {
+                LocalizationManager.ApplyLocalizationParams(
+                    ref fallback,
+                    localParametersRoot,
+                    allowLocalizedParameters
+                );
+            }
+
+            if (LocalizationManager.IsRight2Left & fixForRtl)
+            {
+                fallback = LocalizationManager.ApplyRTLfix(fallback, maxLineLengthForRtl, ignoreRtlNumbers);
+            }
+
+            translation = fallback;
+            return true;
+        }
+
+        return false;
+    }
+
+    private static bool TryGetFallbackFromSource(
+        LanguageSourceData source,
+        string term,
+        out string translation,
+        string overrideSpecialization
+    )
+    {
+        translation = null;
+
+        for (var index = 0; index < source.mLanguages.Count; ++index)
+        {
+            var termData = source.GetTermData(term);
+            if (termData == null)
+            {
+                continue;
+            }
+
+            translation = termData.GetTranslation(index, overrideSpecialization, true);
+            if (!string.IsNullOrEmpty(translation))
+            {
+                return true;
+            }
+        }
+
         return false;
     }
 }
