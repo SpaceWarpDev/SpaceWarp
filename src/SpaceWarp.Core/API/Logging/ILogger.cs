19bd97e6ca6877dea7b89e73f7d43b9448cce6e3
Jan Bureš <jan.bures@live.com>
2024-01-01 15:57:17 +0100

    Fixed SpaceWarp.API.Logging warnings

--- src/SpaceWarp.Core/API/Logging/ILogger.cs
+++ src/SpaceWarp.Core/API/Logging/ILogger.cs
@@ -2,17 +2,52 @@
 
 namespace SpaceWarp.API.Logging;
 
+/// <summary>
+/// Interface for loggers.
+/// </summary>
 [PublicAPI]
 public interface ILogger
 {
+    /// <summary>
+    /// Logs the given object with the given log level.
+    /// </summary>
+    /// <param name="level">Log level.</param>
+    /// <param name="x">Object to log.</param>
     public void Log(LogLevel level, object x);
 
-    public void LogNone(object x);
+    /// <summary>
+    /// Logs the given object with the log level <see cref="LogLevel.Fatal"/>.
+    /// </summary>
+    /// <param name="x">Object to log.</param>
     public void LogFatal(object x);
+
+    /// <summary>
+    /// Logs the given object with the log level <see cref="LogLevel.Error"/>.
+    /// </summary>
+    /// <param name="x">Object to log.</param>
     public void LogError(object x);
+
+    /// <summary>
+    /// Logs the given object with the log level <see cref="LogLevel.Warning"/>.
+    /// </summary>
+    /// <param name="x">Object to log.</param>
     public void LogWarning(object x);
+
+    /// <summary>
+    /// Logs the given object with the log level <see cref="LogLevel.Message"/>.
+    /// </summary>
+    /// <param name="x">Object to log.</param>
     public void LogMessage(object x);
+
+    /// <summary>
+    /// Logs the given object with the log level <see cref="LogLevel.Info"/>.
+    /// </summary>
+    /// <param name="x">Object to log.</param>
     public void LogInfo(object x);
+
+    /// <summary>
+    /// Logs the given object with the log level <see cref="LogLevel.Debug"/>.
+    /// </summary>
+    /// <param name="x">Object to log.</param>
     public void LogDebug(object x);
-    public void LogAll(object x);
 }
