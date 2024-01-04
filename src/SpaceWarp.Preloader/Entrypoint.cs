08c6a0c26d5acdf7ba4df87b0f4ceea8d752c878
Jan Bureš <jan.bures@live.com>
2024-01-04 03:29:20 +0100

    Added preloader and restarter

---
+++ src/SpaceWarp.Preloader/Entrypoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using JetBrains.Annotations;
+
+namespace SpaceWarp.Preloader;
+
+internal static class Entrypoint
+{
+	private static string _gameFolder;
+
+	[UsedImplicitly]
+	public static void Main(string[] args)
+	{
+		_gameFolder = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0])!;
+
+		StartBepinex();
+	}
+
+	private static void StartBepinex()
+	{
+		var bepinexFolder = Path.Combine(_gameFolder, "BepInEx", "core");
+		var bepinexPreloaderPath = Path.Combine(bepinexFolder, "BepInEx.Preloader.dll");
+
+		Assembly.LoadFile(bepinexPreloaderPath);
+		BepInEx.Preloader.Entrypoint.Main();
+	}
+}
